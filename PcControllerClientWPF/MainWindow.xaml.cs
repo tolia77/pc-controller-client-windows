0b1050c4f365880066879f4737e25585249a9417
unknown <tolia.kohan@gmail.com>
2023-01-20 13:06:54 +0200

    Updated

---
+++ PcControllerClientWPF/MainWindow.xaml.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows.Threading;
+using SocketApp;
+
+namespace PcControllerClientWPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MoveMouseButton.IsEnabled = false;
+            OpenLinkButton.IsEnabled = false;
+            CmdExecuteButton.IsEnabled = false;
+            ShutdownPcButton.IsEnabled = false;
+            RestartPcButton.IsEnabled = false;
+            GetScreenshotButton.IsEnabled = false;
+            StreamScreenButton.IsEnabled = false;
+            StopStreamButton.IsEnabled = false;
+        }
+        private bool streamScreen = false;
+        private SocketRequests socketRequests;
+        public void ConnectClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                socketRequests = new(IpTextBox.Text);
+                MoveMouseButton.IsEnabled = true;
+                OpenLinkButton.IsEnabled = true;
+                CmdExecuteButton.IsEnabled = true;
+                ShutdownPcButton.IsEnabled = true;
+                RestartPcButton.IsEnabled = true;
+                GetScreenshotButton.IsEnabled = true;
+                StreamScreenButton.IsEnabled = true;
+            }
+            catch (Exception)
+            {
+                string messageBoxText = $"Error connecting to {IpTextBox.Text}";
+                string caption = "Connection Error";
+                MessageBox.Show(messageBoxText, caption, MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.Yes);
+            }
+        }
+        public void MoveMouseClicked(object sender, EventArgs e)
+        {
+            if
+            (
+            !string.IsNullOrEmpty(RepeatsTextBox.Text) &&
+            !string.IsNullOrEmpty(IntervalTextBox.Text) &&
+            long.TryParse(RepeatsTextBox.Text, out long temp) && temp > 0 &&
+            long.TryParse(IntervalTextBox.Text, out temp) && temp > 0
+            )
+            {
+                try
+                {
+                    string result = socketRequests.MoveMouseRequest(RepeatsTextBox.Text, IntervalTextBox.Text);
+                    if (result == "ERROR")
+                    {
+                        string messageBoxText = $"Trouble on server";
+                        string caption = "Error";
+                        MessageBox.Show(messageBoxText, caption, MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.Yes);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Fatal error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.Yes);
+                }
+            }
+        }
+        public void OpenLinkClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(LinkTextBox.Text))
+            {
+                try
+                {
+                    string result = socketRequests.OpenLinkRequest(LinkTextBox.Text);
+                    if (result == "ERROR")
+                    {
+                        string messageBoxText = $"Trouble on server";
+                        string caption = "Error";
+                        MessageBox.Show(messageBoxText, caption, MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.Yes);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Fatal error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.Yes);
+                }
+            }
+        }
+        public void CmdExecuteClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(CommandTextBox.Text))
+            {
+                try
+                {
+                    string result = socketRequests.CmdExecuteRequest(CommandTextBox.Text);
+                    if (result == "ERROR")
+                    {
+                        string messageBoxText = $"Trouble on server";
+                        string caption = "Error";
+                        MessageBox.Show(messageBoxText, caption, MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.Yes);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Fatal error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.Yes);
+                }
+            }
+        }
+        public void ShutdownPcClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                socketRequests.ShutdownPcRequest();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Fatal error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.Yes);
+            }
+        }
+        public void RestartPcClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                socketRequests.RestartPcRequest();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Fatal error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.Yes);
+            }
+        }
+        public void GetScreenshotClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                using Stream stream = new MemoryStream(socketRequests.GetScreenshotRequest());
+                BitmapImage image = new();
+                stream.Position = 0;
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.StreamSource = stream;
+                image.EndInit();
+                image.Freeze();
+                ScreenshotImage.Source = image;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Fatal error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.Yes);
+            }
+        }
+        public void StreamScreenClicked(object sender, EventArgs e)
+        {
+            StopStreamButton.IsEnabled = true;
+            StreamScreenButton.IsEnabled = false;
+            Thread thread = new(() => StreamScreen());
+            thread.Start();
+        }
+
+        public void StopStreamClicked(object sender, EventArgs e)
+        {
+            streamScreen = false;
+            StopStreamButton.IsEnabled = false;
+            StreamScreenButton.IsEnabled = true;
+        }
+        public void StreamScreen()
+        {
+            streamScreen = true;
+            while (streamScreen)
+            {
+                try
+                {
+                    using Stream stream = new MemoryStream(socketRequests.GetScreenshotRequest());
+                    BitmapImage image = new();
+                    stream.Position = 0;
+                    image.BeginInit();
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.StreamSource = stream;
+                    image.EndInit();
+                    image.Freeze();
+                    Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate
+                    {
+                        ScreenshotImage.Source = image;
+                    });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Fatal error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.Yes);
+                }
+            }
+        }
+    }
+}
---
+++ PcControllerClientWPF/SocketRequests.cs
@@ -0,0 +1,114 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocketApp
+{
+    public class SocketInfo
+    {
+
+        public IPEndPoint MoveMouseEndpoint { get; set; }
+        public IPEndPoint GetScreenshotEndpoint { get; set; }
+        public IPEndPoint OpenLinkEndpoint { get; set; }
+        public IPEndPoint CmdExecuteEndpoint { get; set; }
+        public IPEndPoint ShutdownPcEndpoint { get; set; }
+        public IPEndPoint RestartPcEndpoint { get; set; }
+        public IPAddress IpAddress { get; set; }
+        public Socket AutoSocket
+        {
+            get { return new Socket(IpAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp); }
+            set { }
+        }
+
+        public SocketInfo(IPAddress ipAddress)
+        {
+            IpAddress = ipAddress;
+            MoveMouseEndpoint = new IPEndPoint(IpAddress, 50000);
+            GetScreenshotEndpoint = new IPEndPoint(IpAddress, 50001);
+            OpenLinkEndpoint = new IPEndPoint(IpAddress, 50002);
+            CmdExecuteEndpoint = new IPEndPoint(IpAddress, 50003);
+            ShutdownPcEndpoint = new IPEndPoint(IpAddress, 50004);
+            RestartPcEndpoint = new IPEndPoint(IpAddress, 50005);
+
+        }
+    }
+    public class SocketRequests
+    {
+        private readonly IPAddress ipAddress;
+        private readonly SocketInfo socketInfo;
+        public SocketRequests(string ip)
+        {
+            ipAddress = IPAddress.Parse(ip);
+            socketInfo = new(ipAddress);
+            IPEndPoint endpoint = new(ipAddress, 49999);
+            Socket socketSender = new(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socketSender.Connect(endpoint);
+        }
+        public static byte[] ReceiveAll(Socket SocketSender)
+        {
+            List<byte> buffer = new();
+            while (true)
+            {
+                byte[] currByte = new byte[1];
+                int byteCounter = SocketSender.Receive(currByte, currByte.Length, SocketFlags.None);
+                if (byteCounter == 0)
+                {
+                    return buffer.ToArray();
+                }
+                else
+                {
+                    buffer.Add(currByte[0]);
+                }
+            }
+        }
+        public string MoveMouseRequest(string repeats, string interval)
+        {
+            Socket socketSender = socketInfo.AutoSocket;
+            string data = ""; socketSender.Connect(socketInfo.MoveMouseEndpoint);
+            byte[] msg = Encoding.ASCII.GetBytes($"{repeats} {interval}");
+            socketSender.Send(msg);
+            byte[] bytes = ReceiveAll(socketSender);
+            data += Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+            return data;
+
+        }
+        public string OpenLinkRequest(string link)
+        {
+            Socket socketSender = socketInfo.AutoSocket;
+            string data = ""; socketSender.Connect(socketInfo.OpenLinkEndpoint);
+            byte[] msg = Encoding.ASCII.GetBytes(link);
+            socketSender.Send(msg);
+            byte[] bytes = ReceiveAll(socketSender);
+            data += Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+            return data;
+        }
+        public string CmdExecuteRequest(string command)
+        {
+            Socket socketSender = socketInfo.AutoSocket;
+            string data = ""; socketSender.Connect(socketInfo.CmdExecuteEndpoint);
+            byte[] msg = Encoding.ASCII.GetBytes(command);
+            socketSender.Send(msg);
+            byte[] bytes = ReceiveAll(socketSender);
+            data += Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+            return data;
+        }
+        public string ShutdownPcRequest()
+        {
+            socketInfo.AutoSocket.Connect(socketInfo.ShutdownPcEndpoint);
+            return "SUCCESS";
+        }
+        public string RestartPcRequest()
+        {
+            socketInfo.AutoSocket.Connect(socketInfo.RestartPcEndpoint);
+            return "SUCCESS";
+        }
+        public byte[] GetScreenshotRequest()
+        {
+            Socket socketSender = socketInfo.AutoSocket;
+            socketSender.Connect(socketInfo.GetScreenshotEndpoint);
+            byte[] bytes = ReceiveAll(socketSender);
+            return bytes;
+        }
+    }
+}
